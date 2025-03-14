12667cf1eaae13d125f5ab281835bc13d074da85
Por y para que? <92597680+Obsolic@users.noreply.github.com>
2025-03-14 19:02:41 +0100

    config fixed

--- Config.lua
+++ Config.lua
@@ -1,8 +1,8 @@
 Config = {
     Logs = {
-        ['quitarsancion'] = 'https://discord.com/api/webhooks/1349774064964730963/32IKOjy86FiFLdN9tS3Yhk6-3eBLEjeofVEeTqQLGxlXrqYs23nCHkf_a49zNGzLgB-J',
-        ['apuntarsancion'] = 'https://discord.com/api/webhooks/1349773959088046122/GClRPVSkDOomS0LEKc1h0yhweK5ASsbVlQPL5o_QnAxnS-dtAJAcsphMKh87N5YQMQOw',
-        ['tienesancion'] = 'https://discord.com/api/webhooks/1349774172988899378/lP7Zl6QYJb2X7liBDUoXjF94rEeGvohjMvom12TxHq0Oz1tEtudCGQxK5mvxcSs3vu5E',
+        ['quitarsancion'] = 'WEBHOOK_LINK',
+        ['apuntarsancion'] = 'WEBHOOK_LINK',
+        ['tienesancion'] = 'WEBHOOK_LINK',
     },
     Allowed = {
         'superadmin','admin','mod'
--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # fiveevo_sanciones
  
+# Como utilizar el script?
+
+# Comandos:
+
+```/sancionar license motivo pruebas```
+```/quitarsancion license ```
